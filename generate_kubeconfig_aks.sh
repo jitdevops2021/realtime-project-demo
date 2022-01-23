4c5d0ec74e88c3e9fb5f60a410cb346a884cd00a
jitdevops2021 <83112624+jitdevops2021@users.noreply.github.com>
2022-01-23 18:01:04 +0530

    Update generate_kubeconfig_aks.sh

--- generate_kubeconfig_aks.sh
+++ generate_kubeconfig_aks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x -o errexit -o pipefail
-
+az login --service-principal --username SP_APP_ID --password SP_PASSWD --tenant TENANT_ID
 # generate kubeconfig
 echo "Generating kubeconfig for ${eks_cluster_name}"
 az aks get-credentials \
