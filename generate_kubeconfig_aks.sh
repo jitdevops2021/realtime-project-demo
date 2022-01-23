0796b4814005dc9d0acd6451d8016346fa678289
jitdevops2021 <83112624+jitdevops2021@users.noreply.github.com>
2022-01-23 18:48:22 +0530

    Update generate_kubeconfig_aks.sh

--- generate_kubeconfig_aks.sh
+++ generate_kubeconfig_aks.sh
@@ -6,7 +6,7 @@ echo "${AZURE_CLIENT_SECRET}"
 echo "${AZURE_TENANT_ID}"
 echo "${AZURE_SUBSCRIPTION_ID}"
 
-az login --service-principal --username "${AZURE_CLIENT_ID}" --password "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}
+az login --service-principal --username "${AZURE_CLIENT_ID}" --password "${AZURE_CLIENT_SECRET}" --tenant "${AZURE_TENANT_ID}"
 # generate kubeconfig
 echo "Generating kubeconfig for ${eks_cluster_name}"
 az aks get-credentials \
