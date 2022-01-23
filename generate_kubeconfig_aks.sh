d8a02c9f59e63d01c2075da8615e51748d979610
jitdevops2021 <83112624+jitdevops2021@users.noreply.github.com>
2022-01-23 16:08:14 +0530

    Update generate_kubeconfig_aks.sh

--- generate_kubeconfig_aks.sh
+++ generate_kubeconfig_aks.sh
@@ -7,6 +7,6 @@ echo "Generating kubeconfig for ${eks_cluster_name}"
 az aks get-credentials \
     --name "${eks_cluster_name}" \
     --resource-group "${aws_region}" \
-    --kubeconfig "${KUBECONFIG}"
+    --file "${KUBECONFIG}"
 
 echo "KUBECONFIG saved to ${KUBECONFIG}"
